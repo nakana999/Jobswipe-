83af210a3ef813ae6469c18ac4e7404621a71258
nakana999 <ntakayann9@gmail.com>
2026-05-30 18:52:43 +0000

    “v3”

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,4 @@
-function App(){return null}export default App
+function App() {
+  return <div style={{color:”white”,background:”black”,minHeight:”100vh”,padding:”40px”,fontSize:”32px”}}>JobSwipe is Live!</div>
+}
+export default App
