a40498f2128b49535cb1ef249881909b851f666a
nakana999 <ntakayann9@gmail.com>
2026-05-30 17:23:39 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-function App() {
-  return null
-}
-export default App
+<!DOCTYPE html>
+<html>
+<head><title>JobSwipe</title></head>
+<body style=“background:black;color:white;margin:0;padding:40px;font-family:sans-serif”>
+<h1 style=“font-size:48px”>JobSwipe 🚀</h1>
+<p style=“font-size:20px”>Find your next job by swiping right</p>
+</body>
+</html>
