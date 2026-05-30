85cefd5d3eee8c11bc7b89f1fdafeebc0b01549e
nakana999 <ntakayann9@gmail.com>
2026-05-30 17:03:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
-export default function App(){
-  return React.createElement(“h1”,null,”JobSwipe Live!”)
+var React = require(“react”);
+function App() {
+  return React.createElement(“h1”, null, “JobSwipe Live!”);
 }
+module.exports = App;
