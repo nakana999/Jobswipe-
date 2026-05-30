018eb984a2d1b2bd0dba5fe1614f9975e94da1b1
nakana999 <ntakayann9@gmail.com>
2026-05-30 17:55:52 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
 export default function App() {
-  document.body.style.background = “#070709”
-  document.body.style.color = “white”
-  document.body.style.padding = “40px”
-  document.body.style.fontFamily = “sans-serif”
-  return null
+  return “JobSwipe is Live!”
 }
